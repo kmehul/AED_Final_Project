a6dba439aab79f29fedd72889c146efcc23484b1
anirudhsk2107 <sampathkumar.an@northeastern.edu>
2022-12-07 00:30:40 -0500

    Work queue & request classes

---
+++ EmergencyServicesManagement/src/Services/WorkQueue/WorkRequest.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Services.WorkQueue;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Anirudh
+ */
+public abstract class WorkRequest {
+    private int workId;
+    private int operatorId;
+    private String initDesc;
+    private String assignedTo;
+    private char status;
+    private Date recievedDate;
+    private Date resolvedDate;
+
+    public WorkRequest(){
+        recievedDate = new Date();
+    }
+        
+    public int getWorkId() {
+        return workId;
+    }
+
+    public void setWorkId(int workId) {
+        this.workId = workId;
+    }
+
+    public int getOperatorId() {
+        return operatorId;
+    }
+
+    public void setOperatorId(int operatorId) {
+        this.operatorId = operatorId;
+    }
+
+    public String getInitDesc() {
+        return initDesc;
+    }
+
+    public void setInitDesc(String initDesc) {
+        this.initDesc = initDesc;
+    }
+
+    public String getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(String assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+
+    public char getStatus() {
+        return status;
+    }
+
+    public void setStatus(char status) {
+        this.status = status;
+    }
+
+    public Date getRecievedDate() {
+        return recievedDate;
+    }
+
+    public void setRecievedDate(Date recievedDate) {
+        this.recievedDate = recievedDate;
+    }
+
+    public Date getResolvedDate() {
+        return resolvedDate;
+    }
+
+    public void setResolvedDate(Date resolvedDate) {
+        this.resolvedDate = resolvedDate;
+    }   
+}

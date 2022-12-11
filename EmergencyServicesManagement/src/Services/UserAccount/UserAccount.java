b8ccebc1ed0ac4e7c3779d6bd02516e2a722334c
Tanuj Verma <verma.tanu@northeastern.edu>
2022-12-11 13:24:22 -0500

    added mail functionality

--- EmergencyServicesManagement/src/Services/UserAccount/UserAccount.java
+++ EmergencyServicesManagement/src/Services/UserAccount/UserAccount.java
@@ -5,6 +5,10 @@
 package Services.UserAccount;
 
 import java.util.ArrayList;
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.*;
+
 
 /**
  *
@@ -79,4 +83,42 @@ public class UserAccount {
     public String toString() {
         return userName;
     }
+    
+    
+    public void sendemail()
+    {
+        Properties props = new Properties();    
+          props.put("mail.smtp.host", "smtp.gmail.com");    
+          props.put("mail.smtp.socketFactory.port", "465");    
+          props.put("mail.smtp.socketFactory.class",    
+                    "javax.net.ssl.SSLSocketFactory");    
+          props.put("mail.smtp.auth", "true");    
+          props.put("mail.smtp.port", "465");    
+          //get Session   
+          Session session = Session.getDefaultInstance(props,    
+           new javax.mail.Authenticator() {    
+           protected PasswordAuthentication getPasswordAuthentication() {    
+           return new PasswordAuthentication("verma.tanu@northeastern.edu","Jagdishgeeta@1995");  
+           }    
+          });    
+          //compose message    
+          try {    
+           MimeMessage message = new MimeMessage(session);    
+           message.addRecipient(Message.RecipientType.TO,new InternetAddress("harshsdw1@gmail.com"));    
+           message.setSubject("Test");    
+           message.setText("Txt nahi test");    
+           //send message  
+           Transport.send(message);    
+           System.out.println("message sent successfully");    
+          } catch (MessagingException e) {throw new RuntimeException(e);}    
+    }
+    
+    
+    public static void main(String[] args) {
+        UserAccount ua = new UserAccount();
+        ua.sendemail();
+    }
+    
 }
+
+
